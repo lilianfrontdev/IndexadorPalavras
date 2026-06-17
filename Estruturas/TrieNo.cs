3ac85426fe1dd6661455b17bb1a2a6a169a52619
lilian borsoi <lilianborsoi@icloud.com>
2026-06-17 17:20:17 -0300

    feat: implement manual TrieNode using child-sibling approach

---
+++ Estruturas/TrieNo.cs
@@ -0,0 +1,27 @@
+﻿using IndexadorPalavras.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexadorPalavras.Estruturas
+{
+    public class TrieNo
+    {
+        public char Caracter { get; set; }
+        public bool FimPalavra { get; set; }
+        public TrieNo FilhoEsquerdo { get; set; }
+        public TrieNo IrmaoDireito { get; set; }
+        public ListaOcorrencias Ocorrencias { get; set; }
+
+        public TrieNo(char caracter)
+        {
+            Caracter = caracter;
+            FimPalavra = false;
+            FilhoEsquerdo = null;
+            IrmaoDireito = null;
+            Ocorrencias = new ListaOcorrencias();
+        }
+    }
+}
